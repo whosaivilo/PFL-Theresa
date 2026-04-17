6c24864e43a7a09f348c794c908134366e017e9b
whosaivilo <theresa24si@mahasiswa.pcr.ac.id>
2026-04-17 15:19:31 +0700

    JSON filter dan search

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import './tailwind.css';
 import FrameworkList from "./FrameworkList";
+import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
 
 
 createRoot(document.getElementById("root"))
 .render(
     <div>
-    <FrameworkList/>
+    {/* <FrameworkList/> */}
+    <FrameworkListSearchFilter/>
     </div>
 )
 
