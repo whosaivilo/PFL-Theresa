46b998c57e8741d860ec9e8daf8d796fbf992cbd
whosaivilo <theresa24si@mahasiswa.pcr.ac.id>
2026-05-22 15:28:14 +0700

    chnagen name

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -16,10 +16,7 @@ export default function NotFound() {
       <Link to="/" className="mt-6 rounded-xl bg  -hijau px-6 py-3 font-bold text-white shadow-lg hover:bg-green-600 transition-all">
         Kembali ke Dashboard
       </Link>
-      
-      
-
-      {/* tes */}
+   
     
     </div>
   );
