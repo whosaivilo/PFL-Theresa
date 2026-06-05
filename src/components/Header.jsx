6bbd1dfa30a8024a9cd196c8f6026b9890d4ae4b
whosaivilo <theresa24si@mahasiswa.pcr.ac.id>
2026-06-05 14:49:01 +0700

    hapus akun area chart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,12 +38,7 @@ export default function Header() {
             50
           </span>
         </div>
-        <div
-          id="chart-icon"
-          className="p-3 bg-blue-100 rounded-2xl cursor-pointer"
-        >
-          <FcAreaChart />
-        </div>
+       
         <div
           id="package-icon"
           className="p-3 bg-orange-100 rounded-2xl cursor-pointer"
